ca6780d9e8f19a1541a3a4c9c557713a99a7a314
Роман <vodaz@mail.ru>
2020-05-23 15:32:35 +0300

    Готово

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+    private final Egg6 egg6;
+
+    @Autowired
+    public Duck5(Egg6 egg61) {
+        this.egg6 = egg61;
+    }
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return "в утке — яйцо, " + egg6.toString();
     }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+
+    private final Needle7 needle7;
+
+    @Autowired
+    public Egg6(Needle7 needle71) {
+        this.needle7 = needle71;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return "в яйце — игла,  " + needle7.toString();
     }
 }
